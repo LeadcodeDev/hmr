7b4bd3bde9a1a35655b392537714acb33ff63a12
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2026-05-29 02:07:09 +0200

    refactor: remove IsolateRestartStrategy and consolidate on VmServiceProcessStrategy

--- lib/hmr.dart
+++ lib/hmr.dart
@@ -11,7 +11,6 @@ export 'src/pipeline/filters.dart';
 export 'src/presentation/ansi_presenter.dart';
 export 'src/presentation/json_presenter.dart';
 export 'src/presentation/presenter.dart';
-export 'src/strategies/isolate_restart_strategy.dart';
 export 'src/strategies/run_strategy.dart';
 export 'src/strategies/vm_service_launcher.dart';
 export 'src/strategies/vm_service_process_strategy.dart';
